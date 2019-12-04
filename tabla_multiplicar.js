8ad0fdc117c2fc3acd3bd94bbc64506ac9615872
unknown <samuel.sr.romera@gmail.com>
2019-12-04 12:17:32 +0100

    Solo numeros pares

--- tabla_multiplicar.js
+++ tabla_multiplicar.js
@@ -2,10 +2,12 @@ var numero = parseInt(prompt("Escribe un número"));
 var tabla = document.getElementById("tabla");
 
 if (! isNaN(numero)) {
-	for (var i = 1; i <= 10; i++) {
-		var fila = document.createElement("li");
-		
-		fila.appendChild(document.createTextNode(numero + " * " + i + " = " + (numero * i)));
-		tabla.appendChild(fila);
+	if (numero % 2 === 0) {
+		for (var i = 1; i <= 10; i++) {
+			var fila = document.createElement("li");
+			
+			fila.appendChild(document.createTextNode(numero + " * " + i + " = " + (numero * i)));
+			tabla.appendChild(fila);
+		}
 	}
 }
